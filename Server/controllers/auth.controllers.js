b46ae805399b13ba8351d2aa30ecbefb92c31bad
kimtechnos <franciskimani559@gmail.com>
2024-07-18 12:50:54 +0300

    updating the login in funtionality

---
+++ Server/controllers/auth.controllers.js
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const client = new PrismaClient();
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await client.user.findFirst({
+      where: { email: email },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid login credentials" });
+    }
+
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (passwordMatch) {
+      const payload = {
+        id: user.id,
+        email: user.email,
+        fullName: user.fullName,
+        phoneNumber: user.phoneNumber,
+        role:user.role,
+      };
+      const token = jwt.sign(payload, process.env.JWT_SECRET);
+      res.cookie("access_token", token).json({ success: true, data: payload });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid login credentials" });
+    }
+  } catch (e) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,9 @@
 import express, { response } from "express";
 import { config } from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import usersRouter from "./routes/users.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 config();
 const app = express();
@@ -13,8 +15,11 @@ app.use(
     credentials: true,
   }),
 );
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/users", usersRouter);
+app.use("/api/auth/login", authRouter);
 app.listen(3000, () => {
   console.log("sever is running on port 3000...");
 });
