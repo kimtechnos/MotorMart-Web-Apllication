6002547ebb1ec1499863450371b06b612870efcb
kimtechnos <franciskimani559@gmail.com>
2024-07-20 11:49:32 +0300

    implemented register first user in the front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import "./assets/global.css"
+import Navbar from './components/Common/Navbar'
+import Home from './components/Pages/Home'
+import Contact from './components/Pages/Contact'
+import About from './components/Pages/About'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-    <h2>my cars website</h2>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
---
+++ client/src/components/Common/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import TypingAnimator from "react-typing-animator";
+import "./navbar.css";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const logo = ["Motor", "Mart"];
+
+  return (
+    <div className="Navbar">
+      <span className="nav-logo">
+        <TypingAnimator
+          textArray={logo}
+          loop
+          textColor="yellow"
+          fontSize="2rem"
+        />
+      </span>
+      <div
+        className={`nav-toggle ${isOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="bar"></div>
+      </div>
+      <div className={`nav-items ${isOpen ? "open" : ""}`}>
+        <Link to="/" onClick={handleLinkClick}>
+          Home
+        </Link>
+        <Link to="/about" onClick={handleLinkClick}>
+          About
+        </Link>
+        <Link to="/contact" onClick={handleLinkClick}>
+          Contact
+        </Link>
+        <Link to="/register" className="nav-login" onClick={handleLinkClick}>
+          Register
+        </Link>
+        <Link to="/login" className="nav-signup" onClick={handleLinkClick}>
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
