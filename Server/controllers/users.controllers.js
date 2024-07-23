795bd853972be14b03669af48ed398495f92344f
kimtechnos <franciskimani559@gmail.com>
2024-07-23 14:38:38 +0300

    implemented user update update details section

--- Server/controllers/users.controllers.js
+++ Server/controllers/users.controllers.js
@@ -56,3 +56,28 @@ export const deleteUser = async (req, res) => {
     res.status(500).json({ success: false, message: e.message });
   }
 };
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const { fullName, email, phoneNumber, password } = req.body;
+
+    const updateData = {
+      fullName,
+      email,
+      phoneNumber,
+    };
+
+    if (password) {
+      updateData.password = bcrypt.hashSync(password, 10);
+    }
+    const updatedUser = await prisma.user.update({
+      where: { id: id },
+      data: updateData,
+    });
+
+    res.json({ success: true, data: updatedUser });
+  } catch (e) {
+    res.status(500).json({ success: false, message: e.message });
+  }
+};
--- Server/routes/users.routes.js
+++ Server/routes/users.routes.js
@@ -4,14 +4,17 @@ import {
   getAllusers,
   deleteUser,
   getSingleuser,
+  updateUser,
 } from "../controllers/users.controllers.js";
 import { validateUserInformation } from "../middlewares/user.middleware.js";
 import verifyAdmin from "../middlewares/verifyadmin.js";
+import verifyToken from "../middlewares/verifyToken.middleware.js";
 
 const router = Router();
 
 router.post("/register", validateUserInformation, createUser);
 router.get("/", verifyAdmin, getAllusers);
+router.patch("/update/:id", verifyToken, updateUser);
 router.get("/:id", verifyAdmin, getSingleuser);
 router.delete("/:id", verifyAdmin, deleteUser);
 
