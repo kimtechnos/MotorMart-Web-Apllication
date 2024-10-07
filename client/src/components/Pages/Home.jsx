76df84d15ac49b013989d1d1bb6cfed9e6a2155a
kimtechnos <franciskimani559@gmail.com>
2024-10-07 17:52:33 +0300

    updating the home section page

--- client/src/components/Pages/Home.jsx
+++ client/src/components/Pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+//import homeImage from "../../assets/mg6.png";
+import homeImage from "../../assets/mg6.png";
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
+    <div className="home">
+      <div className="secContainer">
+        <div className="homeText">
+          <span className="homeSpan">Meet your new car</span>
+          <h1 className="homeTitle">MG6 Typr new</h1>
+          <div className="btns">
+            <button className="btn">More Details</button>
+            <button className="btn primaryBtn">Test Drive</button>
+          </div>
+        </div>
+      </div>
+      <div className="homeImage">
+        <img src={homeImage} alt="homeImage" />
+      </div>
     </div>
   );
 };
