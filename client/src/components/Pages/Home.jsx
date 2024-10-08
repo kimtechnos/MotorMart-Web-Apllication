f68e6d7888ec98ab6348c5ed392fde0eab535ca9
kimtechnos <franciskimani559@gmail.com>
2024-10-08 15:50:47 +0300

    updating the home page search dashbord

--- client/src/components/Pages/Home.jsx
+++ client/src/components/Pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="secContainer">
         <div className="homeText">
           <span className="homeSpan">Meet your new car</span>
-          <h1 className="homeTitle">MG6 Typr new</h1>
+          <h1 className="homeTitle">MG6 Type </h1>
           <div className="btns">
             <button className="btn">More Details</button>
             <button className="btn primaryBtn">Test Drive</button>
@@ -19,6 +19,32 @@ const Home = () => {
       <div className="homeImage">
         <img src={homeImage} alt="homeImage" />
       </div>
+      <section className="search">
+        <div className="secContainer container">
+          <h3 className="title">Which vehicle are you looking for?</h3>
+          <div className="searchDiv">
+            <div className="inputWrapper">
+              <label htmlFor="vehicleType">Type</label>
+              <input type="text" id="vehicleType" placeholder="Type" />
+            </div>
+            <div className="inputWrapper">
+              <label htmlFor="vehicleYear">Year</label>
+              <input type="number" id="vehicleYear" placeholder="Year" />
+            </div>
+            <div className="inputWrapper">
+              <label htmlFor="vehicleModel">Model</label>
+              <input type="text" id="vehicleModel" placeholder="Model" />
+            </div>
+            <div className="inputWrapper">
+              <label htmlFor="vehiclePrice">Price</label>
+              <input type="number" id="vehiclePrice" placeholder="Price" />
+            </div>
+            <button className="btn primaryBtn">
+              <span>Search</span>
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
