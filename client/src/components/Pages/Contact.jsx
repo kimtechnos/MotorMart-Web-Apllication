0c33e98813c6ac88b7c139d034ce823367ee0838
kimtechnos <franciskimani559@gmail.com>
2024-10-10 11:39:32 +0300

    setting up firebase contact us form

--- client/src/components/Pages/Contact.jsx
+++ client/src/components/Pages/Contact.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import "./Contact.scss";
 
-const Contact = () => {
+function Contact() {
+  const handleSubmit = () => {};
   return (
-    <div>
-      <br />
-      <br />
-      <br /> <br />
-      contact
-    </div>
+    <section className="contact-section">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-10">
+            <div className="wrapper">
+              <div className="row no-gutters">
+                <div className="col-md-6">
+                  <div className="contact-wrap w-100 p-lg-5 p-4">
+                    <br />
+                    <br />
+                    <h3 className="mb-4">send us a message</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-};
+}
 
 export default Contact;
